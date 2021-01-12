7d34fca5dcea9fd7ddc5e9d26d7c4e44bbb427c5
sjoholm123 <70257113+sjoholm123@users.noreply.github.com>
2021-01-12 15:31:04 +0100

    Update read.php

--- html/read.php
+++ html/read.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/readStyle.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Marvel&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
 <?php
 
 if(!isset($_SERVER['HTTP_REFERER'])){
@@ -13,17 +26,12 @@ if(!isset($_SERVER['HTTP_REFERER'])){
       $_SESSION['password'];
 
    $API = $_SESSION['API'];
+
 } else {
    header('Location: index.html');
 }
 
-$pageID = $_GET['pageID'];      // GET genom form eller href 
-
-    /*    $data = array(
-        'pageID' => ''.$pageID          
-    );
-    */ //datan som ska postas med i arrayen
-
+$pageID = $_GET['pageID'];   // GET genom form eller href 
     
   // payloaden behövs när du ska posta data, $payload = json_encode($data);
     $ch = curl_init("https://wider.ntigskovde.se/api/pages/read_post_page.php?API=$API&pageID=$pageID");    //kolla så att filsökvägen är rätt /api/*/*.php?API=$API
@@ -33,18 +41,22 @@ $pageID = $_GET['pageID'];      // GET genom form eller href
 
     $result = curl_exec($ch);
     curl_close($ch);
-/*
-    if($result == 'nono'){
-        header('location: index.html');
-    }
-    else{
-        $_SESSION['API'] = trim($result);
-        $_SESSION['loggedin'] = true;
-        $_SESSION['username'] = $username;
-        $_SESSION['password'] = $password;
-        //skicka till loggedin.php
-        header('location: loggedin.php');
-    }
-    */
-    echo $result;  // echo $result för att kolla om executen funka
+   
+    //$json = json_encode($result)
+  $json = json_decode($result, true);
+
+  echo '<div class="postTitle">';
+  echo $json['posts']['0']['postTitle'];
+  echo '</div>';
+  echo $json['posts']['0']['pText'];
+
 ?>
+</body>
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <script src="js/highlight.js"></script>
+<script src="js/popUpLocal.js"></script>
+<script src="js/alert.js"></script>
+<script type="text/javascript" src="js/marvel.js"></script>
+<script src="js/menu.js"></script>
+</html>
